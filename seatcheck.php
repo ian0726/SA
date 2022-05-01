2c9a60f0a7d0f922c761bb757ab0e6453ad75218
HsiYuSmooth <93332741+HsiYuSmooth@users.noreply.github.com>
2022-05-01 23:00:20 +0800

    Add files via upload

---
+++ seatcheck.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>座位測試</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Bootstrap Icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+<style>
+    body{
+            background-color: rgb(241, 242, 243);
+        }
+    .title{
+        text-align:center;
+    }
+    table{
+        margin-left:auto;
+        margin-right:auto;
+    }
+    .seat_id1{
+        font-size:2rem;
+    }
+    .seat_id2{
+        font-size:2rem;
+    }
+    .seat_id3{
+        font-size:2rem;
+    }
+    .seat_id4{
+        font-size:2rem;
+    }
+    .seat_id5{
+        font-size:2rem;
+    }
+    .seat_id6{
+        font-size:2rem;
+    }
+    .seat_id7{
+        font-size:2rem;
+    }
+    .seat_id8{
+        font-size:2rem;
+    }
+    .seat_id9{
+        font-size:2rem;
+    }
+    .seat_id10{
+        font-size:2rem;
+    }
+    .seat_id11{
+        font-size:2rem;
+    }
+    .seat_id12{
+        font-size:2rem;  
+    }
+    .seat_id13{
+        font-size:2rem;
+
+    }
+    .seat_id14{
+        font-size:2rem;
+    }
+    .seat_id15{
+        font-size:2rem;
+    }
+    .seat_id16{
+        font-size:2rem;
+    }
+    .seat_id17{
+        font-size:2rem;
+
+    }
+    .seat_id18{
+        font-size:2rem;
+    }
+    .seat_id19{
+        font-size:2rem;
+    }
+    .seat_id20{
+        font-size:2rem;
+    }
+    .seat_id21{
+        font-size:2rem;
+
+    }
+    .seat_id22{
+        font-size:2rem;
+    }
+    .seat_id23{
+        font-size:2rem;
+    }
+    .seat_id24{
+        font-size:2rem;
+    }
+    .seat_id25{
+        font-size:2rem;
+    }
+    .seat_id26{
+        font-size:2rem;
+    }
+    .btn-showaiting{
+        margin-left:700px;
+        margin-bottom:10px;
+    }
+    .btn-showaiting{
+    width: 150px;
+    height: 40px;
+    text-align: center;
+    border-radius: 360px;
+    cursor: pointer;
+    line-height: 30px;
+    color: #000;
+    background-color: transparent;
+    border: 1px solid #000;
+    transition: all .3s linear;
+    }
+    .btn-showaiting:hover{
+        background-color: #202427;
+        color: #fff;
+    }
+    .btn-notification{
+        margin-left:1000px;
+        margin-bottom:10px;
+    }
+    .btn-notification{
+    width: 150px;
+    height: 40px;
+    text-align: center;
+    border-radius: 360px;
+    cursor: pointer;
+    line-height: 30px;
+    color: #000;
+    background-color: transparent;
+    border: 1px solid #000;
+    transition: all .3s linear;
+    }
+    .btn-notification:hover{
+        background-color: #202427;
+        color: #fff;
+    }
+</style>
+</head>
+<body>
+    <h2 class="title">座位</h2>
+    <table>
+        <tr>
+            <td>單人1桌<p class="seat_id1"><i class="bi bi-square"></i></p></td>
+        </tr>
+        <tr>
+            <td>單人2桌<p class="seat_id2"><i class="bi bi-square"></i></p></td>
+            <td>雙人13桌<p class="seat_id13"><i class="bi bi-square"></i></p></td>
+            <td>雙人14桌<p class="seat_id14"><i class="bi bi-square"></i></p></td>
+            <td>雙人15桌<p class="seat_id15"><i class="bi bi-square"></i></p></td>
+            <td>雙人16桌<p class="seat_id16"><i class="bi bi-square"></i></p></td>
+        </tr>
+        <tr>
+            <td>單人3桌<p class="seat_id3"><i class="bi bi-square"></i></p></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>沙發25桌<p class="seat_id25"><i class="bi bi-square"></i></p></td>
+        </tr>
+        <tr>
+            <td>單人4桌<p class="seat_id4"><i class="bi bi-square"></i></p></td>
+            <td>雙人17桌<p class="seat_id17"><i class="bi bi-square"></i></p></td>
+            <td>雙人18桌<p class="seat_id18"><i class="bi bi-square"></i></p></td>
+            <td>雙人19桌<p class="seat_id19"><i class="bi bi-square"></i></p></td>
+            <td>雙人20桌<p class="seat_id20"><i class="bi bi-square"></i></p></td>
+        </tr>
+        <tr>
+            <td>單人5桌<p class="seat_id5"><i class="bi bi-square"></i></p></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>   
+            <td>沙發26桌<p class="seat_id26"><i class="bi bi-square"></i></p></td>
+        </tr>
+        <tr>
+            <td>單人6桌<p class="seat_id6"><i class="bi bi-square"></i></p></td>
+            <td>雙人21桌<p class="seat_id21"><i class="bi bi-square"></i></p></td>
+            <td>雙人22桌<p class="seat_id22"><i class="bi bi-square"></i></p></td>
+            <td>雙人23桌<p class="seat_id23"><i class="bi bi-square"></i></p></td>
+            <td>雙人24桌<p class="seat_id24"><i class="bi bi-square"></i></p></td>
+        </tr>
+        <tr>
+            <td>單人7桌<p class="seat_id7"><i class="bi bi-square"></i></p></td>
+        </tr>
+        <tr>
+            <td>單人8桌<p class="seat_id8"><i class="bi bi-square"></i></p></td>
+            <td>單人9桌<p class="seat_id9"><i class="bi bi-square"></i></p></td>
+            <td>單人10桌<p class="seat_id10"><i class="bi bi-square"></i></p></td>
+            <td>單人11桌<p class="seat_id11"><i class="bi bi-square"></i></p></td>
+            <td>單人12桌<p class="seat_id12"><i class="bi bi-square"></i></p></td>
+        </tr>
+    </table>
+
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-showaiting" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    查看候位狀態
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">候位組數顯示</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            您目前的候位順序為:
+            <br>
+            尚須等待 組
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">了解</button>
+        </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
