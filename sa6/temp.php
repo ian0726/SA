65e59e1d0a3b7b727376c79b20661ebff3c08441
chennyday <80695174+chennyday@users.noreply.github.com>
2022-05-26 11:26:12 +0800

    Delete sa6 directory

--- sa6/temp.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    include("db.php");
-    $user_id = "fff"
-    $sql = "SELECT * FROM `queue` WHERE user_id = ".$user_id."";
-    $rs = mysqli_fetch_assoc($con, $sql);
-?>
